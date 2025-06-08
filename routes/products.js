f432b004adb7e092eaf8d5ccbfb861b5322ba943
ayushmathur12 <ayush@itinker.io>
2025-06-08 16:10:54 +0530

    added validation

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {addProduct, getProducts} = require('../controller/products');
 const verifyAuth = require('../middleware/authmiddleware');
 
-router.post('/addProducts', addProduct);
+router.post('/addProducts', verifyAuth, addProduct);
 router.get('/productDetails', getProducts);
 
 module.exports = router;
